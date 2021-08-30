57bcbc3bee3103cf7d2aa227e51ccd4826038720
deoagent <deoagent@gmail.com>
2021-08-31 03:54:47 +0800

    #Blade Templating & Compiling Assets

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-       <h1>Welcome to Laravel</h1>
+       <div class="jumbotron text-center">
+             <h1>Welcome To Laravel!</h1>
        <p>This is the laravel application from the "Laravel From Scratch " Youtube Series</p>
+       <p><a class="btn btn-primary btn-lg" role="button" href="/login">Login</a> <a class="btn btn-success btn-lg" href="/register">Register</a></p>
+       </div>
  @endsection
